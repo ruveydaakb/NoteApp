0b6c6e731d980c8b98d9c7b0c0231baf18338f93
Rüveyda Akbolat <54941922+ruveydaakb@users.noreply.github.com>
2023-04-01 20:36:45 +0300

    Add files via upload

---
+++ NoteApp/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import { NotesContext } from "../contexts/notesContext";
+import { useContext } from "react";
+
+const Navbar = () => {
+    const { notes } = useContext(NotesContext);
+    return (
+        <div className="navbar">
+            <h1>NoteApp</h1>
+            <p> Toplam { notes.length } adet not mevcut.</p>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ NoteApp/src/components/NoteApp.js
@@ -0,0 +1,20 @@
+import Navbar from "./Navbar";
+import NoteList from "./NoteList";
+import AddNote from "./AddNote";
+
+import NotesContextProvider from "../contexts/notesContext";
+
+const NoteApp = () => {
+    
+    return (
+        <NotesContextProvider>
+            <div className="container">
+                <Navbar />
+                <NoteList />
+                <AddNote />
+            </div>
+        </NotesContextProvider>
+    );
+};
+
+export default NoteApp;
